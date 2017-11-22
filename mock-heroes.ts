02ebc5ffcb43c2d48eac3a23b6218c08bed55b76
priydarshinivarnika <33860949+priydarshinivarnika@users.noreply.github.com>
2017-11-22 08:08:58 +0530

    Add files via upload

---
+++ mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 101, name: 'Dhyan', skill: 'java' },
+  { id: 102, name: 'Murali' , skill: 'java' },
+  { id: 103, name: 'Uma' , skill: 'java' },
+  { id: 104, name: 'Srirekha' , skill: 'java' },
+  { id: 105, name: 'Ajaya' , skill: 'java' },
+  { id: 106, name: 'Desikan' , skill: 'java' },
+  { id: 107, name: 'Bhavesh' , skill: 'java' },
+  { id: 108, name: 'Satish' , skill: 'java' },
+  { id: 109, name: 'Jaison' , skill: 'java' },
+  { id: 110, name: 'Varnika' , skill: 'java' }
+];
